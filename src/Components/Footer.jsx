15169f0fa059b6796114b7e6c1082d7515fc6334
manuelthegoat <manuelcreation247@gmail.com>
2024-06-14 14:48:31 +0100

    gg

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+import one from "../Assets/ig.png";
+import two from "../Assets/fb.png";
+import three from "../Assets/twitter.png";
+import four from "../Assets/yt.png";
 
 const Footer = () => {
   return (
     <>
-
+      <div className="footer">
+        <div className="input">
+          <input type="text" name="" id="" placeholder="Email Address" />
+          <button className="accept">(""")</button>
+        </div>
+        <div className="footlinks">
+          <div className="footleft">
+            <p className="foottitle">ABOUT DE MIRES</p>
+            <div className="footerlinks">
+              <p>contact us</p>
+              <p>contact us</p>
+              <p>contact us</p>
+            </div>
+          </div>
+          <div className="footright">
+            <p className="foottitle">MENU</p>
+            <div className="footerlinks">
+              <p>drinks</p>
+              <p>drinks</p>
+              <p>drinks</p>
+            </div>
+          </div>
+        </div>
+        <div className="connect">
+          <p className="connecttitle">CONNECT WITH US</p>
+          <div className="icons">
+            <img src={one} alt="" />
+            <img src={two} alt="" />
+            <img src={three} alt="" />
+            <img src={four} alt="" />
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,18 +14,10 @@ function BasicExample() {
         <Navbar.Collapse id="basic-navbar-nav" >
           <Nav className="me-auto">
             <Nav.Link href="#home" className="text-white">Home</Nav.Link>
-            <Nav.Link href="#link" className="text-white">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link" className="text-white">Food</Nav.Link>
+            <Nav.Link href="#link" className="text-white">Drinks</Nav.Link>
+            <Nav.Link href="#link" className="text-white">Reserve</Nav.Link>
+           
           </Nav>
         </Navbar.Collapse>
       </Container>
