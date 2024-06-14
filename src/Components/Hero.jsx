50b44c7544dc5660a417bc78b705c3f417b8632a
manuelthegoat <manuelcreation247@gmail.com>
2024-06-14 15:27:24 +0100

    gg

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -12,31 +12,13 @@ const Hero = () => {
   return (
     <>
       <div className="hero">
-        <Swiper
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={50}
-          slidesPerView={3}
-          navigation
-          pagination={{ clickable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-          className="content"
-        >
-          <SwiperSlide>
+        <div className="content">
+          <div>
             <p className="title">Our Tasty JOllof</p>
             <p className="text">Savor the essence of West Africa.</p>
             <button className="button">SEE MENU</button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p className="title">Have An Upcoming Event?</p>
-            <button className="button2">SEE MENU</button>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p className="title">Our Tasty JOllof</p>
-            <p className="text">Savor the essence of West Africa.</p>
-            <button className="button">SEE MENU</button>
-          </SwiperSlide>
-        </Swiper>
+          </div>
+        </div>
       </div>
     </>
   );
